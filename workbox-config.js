87ac637a941c43b26c095bc368e9ebc19808c3c8
MAd SAge <59429196+MAdSAge@users.noreply.github.com>
2024-10-06 15:31:52 +0530

    hope this works

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,44 @@
 module.exports = {
     globDirectory: './', // The directory to cache files from
     globPatterns: [
-        '**/*.{html,css,js}', // Files to cache
+        '**/*.{html,css,js,png}', // Files to cache
     ],
     swDest: 'sw.js', // Output service worker file
     clientsClaim: true, // Take control of uncontrolled clients
     skipWaiting: true, // Force the waiting service worker to become the active service worker
+    runtimeCaching: [
+        {
+            urlPattern: ({ request }) => request.destination === 'document', // Cache HTML files
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'html-cache',
+                expiration: {
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 Days
+                },
+            },
+        },
+        {
+            urlPattern: ({ request }) => request.destination === 'script', // Cache JS files
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'js-cache',
+                expiration: {
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 Days
+                },
+            },
+        },
+        {
+            urlPattern: ({ request }) => request.destination === 'style', // Cache CSS files
+            handler: 'StaleWhileRevalidate',
+            options: {
+                cacheName: 'css-cache',
+                expiration: {
+                    maxEntries: 50,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 Days
+                },
+            },
+        },
+    ],
 };
